94494cdcc16d01509c3b838515a83f2eba0e2107
Yuriy Dvorzhetskiy <ydvorzhetskiy@xperts-bay.ru>
2021-04-05 22:55:59 +0600

    event-loop

---
+++ event-loop/fs.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+fs.readFile('./events.js', () => { 
+  console.log('fs') 
+  setTimeout(() => console.log('timeout')) 
+  setImmediate(() => console.log('immediate'))
+})
---
+++ event-loop/next-tick-order-dimistified.js
@@ -0,0 +1,53 @@
+const next = () => console.log('next')
+const timeout = (index) => console.log(`timeout ${index}`)
+const immediate = (index) => console.log(`immediate ${index}`)
+
+setTimeout(() => {
+  timeout(1)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  timeout(2)
+})
+setTimeout(() => {
+  timeout(3)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  timeout(4)
+})
+setTimeout(() => {
+  timeout(5)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  timeout(6)
+})
+
+setImmediate(() => {
+  immediate(1)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  immediate(2)
+})
+setImmediate(() => {
+  immediate(3)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  immediate(4)
+})
+setImmediate(() => {
+  immediate(5)
+  
+  process.nextTick(next)
+  process.nextTick(next)
+  
+  immediate(6)
+})
